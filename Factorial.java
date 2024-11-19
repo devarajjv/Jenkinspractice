34bd7788e864c604d74cdc76badae9aef71fe1c8
devarajjv <devarajjv41@gmail.com>
2024-11-19 20:36:33 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,26 @@
+
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n =5;
+		
+		int result = fact(n);
+		
+		System.out.println(result);
+		
+
+	}
+	
+	public static int fact(int n) {
+		
+		if(n==0) {
+			return 1;
+		}
+		
+		return (n*fact(n-1));
+	}
+
+}
